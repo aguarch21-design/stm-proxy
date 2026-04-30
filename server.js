b242fe83d3fb3a4fb3a4a82df7418c1062a44277
aguarch21-design <aguarch21@gmail.com>
2026-04-30 10:59:41 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.post('/api/buses', async (req, res) => {
+  try {
+    const r = await fetch('http://www.montevideo.gub.uy/buses/rest/stm-online', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body || {})
+    });
+    res.json(await r.json());
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+app.listen(process.env.PORT || 3001);
