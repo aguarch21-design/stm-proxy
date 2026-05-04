57b9db9cd732e93014ddf2a240fd69fe8d88c8c9
aguarch21-design <aguarch21@gmail.com>
2026-05-04 07:43:15 -0300

    Serve reporte-buses-stm.html on root and specific route
    Added routes to serve the HTML file for the bus report.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reporte-buses-stm.html'));
+});
+app.get('/reporte-buses-stm.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reporte-buses-stm.html'));
+});
 app.post('/api/buses', async (req, res) => {
   try {
     const r = await fetch('http://www.montevideo.gub.uy/buses/rest/stm-online', {
